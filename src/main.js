9aec209d41ee23656300c7c0ad1f44131956ba69
Joey Andres <jandres@ualberta.ca>
2016-11-02 18:06:53 -0600

    Basic express server.

--- src/main.js
+++ src/main.js
@@ -19,40 +19,19 @@ function main() {
     let pointsOfInterest = new POI(mongodPort, 'app');
 
     pointsOfInterest.open();
-
-    let dataOfInterest = graphiteAdapter.render.render({
-        target: 'removeBelowValue(invidi.webapp.localhost_localdomain.request.*.mean, 5)',
-        format: 'json',
-        from: '11:00_20160917',
-        until: '22:00_20160917',
-        yMin: 5
-    });
-    dataOfInterest.forEach(data => {
-        data.datapoints = data.datapoints.filter(point => {
-            return !_.isNull(point[0]);
-        });
-    });
-
-    // Playing with threshold pattern.
-    // TODO: Target should be able to take graphite wildcards. Let's try to achieve atomic features for now.
-    // todo: array checks for threshold.
-    // todo: Pattern is parameter to points-of-interest.
-    let threshold = new Threshold([{
-        target: 'invidi.webapp.localhost_localdomain.request.total_response_time.mean',
-        thresholdRule: THRESHOLDRULE.GREATERTHAN,
-        value: 5
-    }, {
-        target: 'invidi.webapp.localhost_localdomain.request.PlaylistRequest.mean',
-        thresholdRule: THRESHOLDRULE.GREATERTHAN,
-        value: 5
-    }]);
-    console.log('Testing error:');
-    console.log(threshold.error(dataOfInterest) == 0);
-    console.log('End Testing error***');
-
-    pointsOfInterest.insert([threshold]);
-    pointsOfInterest.removeAll();
     pointsOfInterest.close();
+
+    app.post('/pattern/threshold', (req, res) => {
+        console.log(req);
+    });
+
+    app.get('/pattern', (req, res) => {
+        res.json([]);
+    });
+
+    app.listen(3000, function () {
+        console.log('Example app listening on port 3000!')
+    })
 }
 
 // Here we run the main executable.
