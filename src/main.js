d7c299ee98fca3eff774447faa61d7022a70dc27
Joey Andres <jandres@ualberta.ca>
2016-11-04 03:53:15 -0600

    Log threshold error marking.

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import RenderAPIAdapter from './render-api-adapter';
 
 import GraphiteAdapter from './graphite-adapter';
 import POI from './points-of-interest';
-//import {THRESHOLDRULE, Threshold} from './patterns/threshold';
+import {THRESHOLDRULE, Threshold} from './patterns/threshold';
 
 // This will be the main executable.
 function main() {
@@ -24,8 +24,38 @@ function main() {
 
   pointsOfInterest.open();
 
-  app.post('/pattern/threshold', (req, res) => {
-    req.body.
+  app.post('/pattern/threshold', (req, res, next) => {
+    let rawThreshold = req.body.threshold;
+    rawThreshold = rawThreshold.map(threshold => {
+      switch (String(threshold.thresholdRule).trim()) {
+        case '<':
+          threshold.thresholdRule = THRESHOLDRULE.LESSTHAN;
+          break;
+        case '<=':
+          threshold.thresholdRule = THRESHOLDRULE.LESSTHANEQUAL;
+          break;
+        case '=':
+          threshold.thresholdRule = THRESHOLDRULE.EQUAL;
+          break;
+        case '>=':
+          threshold.thresholdRule = THRESHOLDRULE.GREATERTHANEQUAL;
+          break;
+        case '>':
+          threshold.thresholdRule = THRESHOLDRULE.GREATERTHAN;
+          break;
+        default:
+          var err = new Error('Unknown threshold rule.');
+          err.status = 500;
+          next(err);
+      }
+
+      return threshold;
+    });
+
+    let threshold = new Threshold(rawThreshold);
+    pointsOfInterest.insert([threshold]);
+
+    res.send('Success');
   });
 
   app.get('/pattern/threshold', (req, res) => {
