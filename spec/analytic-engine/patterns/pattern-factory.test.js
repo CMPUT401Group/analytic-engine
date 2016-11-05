587e9d05779406ece895116fcbcf8cc652cf8c4a
reverseoni <fierce_deity888@hotmail.com>
2016-11-04 19:05:03 -0600

    Edited docs for pattern-factory.test.js

--- spec/analytic-engine/patterns/pattern-factory.test.js
+++ spec/analytic-engine/patterns/pattern-factory.test.js
@@ -1,5 +1,19 @@
 import {Threshold, THRESHOLDRULE, PatternFactory} from '../../../src/patterns';
 
+/**
+ * @description Test suite for pattern-factory.js:
+ * Utilizing jasmine as our Javascript testing framework
+ * {@tutorial https://jasmine.github.io/2.5/introduction}
+ */
+
+
+/**
+ * @description Testing implements and uses the serialization and
+ * deserialization of the pattern classes inheriting from patterns.js.
+ * Tests whether data is being properly read and stored back to its
+ * expected serialized and deserialized state.
+ * Currently implements only threshold pattern
+ */
 describe("AnalyticEngine - Patterns - PatternFactory", function() {
     it ('Given a serialized Threshold, spits out a Threshold instance', function() {
         let serializedThreshold = {  // An object so we can save it in mongodb as a single object.
